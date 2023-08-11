d7c98d192f11014448495fb6afdfef3a528a1a91
gky360 <gky360@gmail.com>
2023-08-11 09:56:38 +0900

    Add q2_2

---
+++ q2_2/q2_2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func main() {
+	w1 := csv.NewWriter(os.Stdout)
+	w1.Write([]string{"hoge", "fuga"})
+	w1.Flush()
+
+	file, err := os.Create("./tmp/q2_2.txt")
+	if err != nil {
+		panic(err)
+	}
+	w2 := csv.NewWriter(file)
+	w2.Write([]string{"hoge", "fuga"})
+	w2.Flush()
+}
