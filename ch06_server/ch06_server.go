a59a500fe831c60e4c633c0aa12ef99b4c34f10c
gky360 <gky360@gmail.com>
2023-08-26 18:24:59 +0900

    Chap 6.8.1

--- ch06_server/ch06_server.go
+++ ch06_server/ch06_server.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"bufio"
-	"bytes"
-	"compress/gzip"
 	"fmt"
 	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
-	"time"
 )
+
+var contents = []string{
+	"これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
+	"むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
+	"中山さまというおとのさまが、おられたそうです。",
+	"その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
+	"ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
+	"そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
+}
 
 func isGzipAcceptable(request *http.Request) bool {
 	return strings.Index(
@@ -23,14 +29,9 @@ func processSession(conn net.Conn) {
 	fmt.Printf("Accept %v\n", conn.RemoteAddr())
 	defer conn.Close()
 	for {
-		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		request, err := http.ReadRequest(bufio.NewReader(conn))
 		if err != nil {
-			neterr, ok := err.(net.Error)
-			if ok && neterr.Timeout() {
-				fmt.Println("Timeout")
-				break
-			} else if err == io.EOF {
+			if err == io.EOF {
 				break
 			}
 			panic(err)
@@ -42,27 +43,39 @@ func processSession(conn net.Conn) {
 		}
 		fmt.Println(string(dump))
 
-		response := http.Response{
-			StatusCode: 200,
-			ProtoMajor: 1,
-			ProtoMinor: 1,
-			Header:     make(http.Header),
+		fmt.Fprintf(conn, strings.Join([]string{
+			"HTTP/1.1 200 OK",
+			"Content-Type: text/plain",
+			"Transfer-Encoding: chunked",
+			"", "",
+		}, "\r\n"))
+		for _, content := range contents {
+			bytes := []byte(content)
+			fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bytes), content)
 		}
-		if isGzipAcceptable(request) {
-			content := "Hello, world (gzipped)\n"
-			var buffer bytes.Buffer
-			writer := gzip.NewWriter(&buffer)
-			io.WriteString(writer, content)
-			writer.Close()
-			response.Body = io.NopCloser(&buffer)
-			response.ContentLength = int64(buffer.Len())
-			response.Header.Set("Content-Encoding", "gzip")
-		} else {
-			content := "Hello, world\n"
-			response.Body = io.NopCloser(strings.NewReader(content))
-			response.ContentLength = int64(len(content))
-		}
-		response.Write(conn)
+		fmt.Fprintf(conn, "0\r\n\r\n")
+
+		// response := http.Response{
+		// 	StatusCode: 200,
+		// 	ProtoMajor: 1,
+		// 	ProtoMinor: 1,
+		// 	Header:     make(http.Header),
+		// }
+		// if isGzipAcceptable(request) {
+		// 	content := "Hello, world (gzipped)\n"
+		// 	var buffer bytes.Buffer
+		// 	writer := gzip.NewWriter(&buffer)
+		// 	io.WriteString(writer, content)
+		// 	writer.Close()
+		// 	response.Body = io.NopCloser(&buffer)
+		// 	response.ContentLength = int64(buffer.Len())
+		// 	response.Header.Set("Content-Encoding", "gzip")
+		// } else {
+		// 	content := "Hello, world\n"
+		// 	response.Body = io.NopCloser(strings.NewReader(content))
+		// 	response.ContentLength = int64(len(content))
+		// }
+		// response.Write(conn)
 	}
 }
 
